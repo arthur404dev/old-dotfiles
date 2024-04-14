57d7eb4538f2df4b925fd08f15be8d5465f06007
Arthur Andrade <git@arthur404.dev>
2024-04-13 23:47:50 -0300

    feat: add zsh configs and add wslu

--- .zsh/nvm.zsh
+++ .zsh/nvm.zsh
@@ -1,3 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
 # NVM lazy load
 if [ -s "$HOME/.nvm/nvm.sh" ]; then
   [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
