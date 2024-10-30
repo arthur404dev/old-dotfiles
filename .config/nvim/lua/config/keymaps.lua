11d0c87238d5056a56cc314dd0e2ed80e943b6d1
Arthur Andrade <git@arthur404.dev>
2024-10-30 02:26:39 -0300

    feat: add configs for new tools
    add configuration for bat
    add lazygit theme
    add delta as diff engine
    update nvim config
    update aliases and functions

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.keymap.del("n", "<leader>w|")
+-- vim.keymap.del("n", "<leader>w|")
 vim.keymap.set("n", "<leader>w\\", "<C-W>v", { desc = "Split window right", remap = true })
-vim.keymap.del("n", "<leader>|")
+-- vim.keymap.del("n", "<leader>|")
 vim.keymap.set("n", "<leader>\\", "<C-W>v", { desc = "Split window right", remap = true })
--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,5 +1,5 @@
 # Colormap
-function colormap() {
+colormap() {
   for i in {0..255}; do 
     print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f "
     [ $((i % 6)) -eq 5 ] && echo
@@ -10,3 +10,8 @@ function colormap() {
 if_exists() {
     command -v $1 >/dev/null 2>&1
 } 
+
+# Searches recursively in a folder / file and pipes into fzf
+search() {
+  grep -r "$1" "$2" | fzf
+}
