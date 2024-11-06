6986381a9cce047e836da7e01a088fa39f3febf0
Arthur Andrade <git@arthur404.dev>
2024-11-06 02:41:48 -0300

    feat: add plugins and config for zinit
    change configuration to use zinit to set and configure plugins for zsh

--- .zsh/exporters.zsh
+++
@@ -1,3 +0,0 @@
-# New exports
-export PATH=$PATH:$(go env GOPATH)/bin
-export PATH=$PATH:$(go env GOROOT)/bin
--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,14 +1,19 @@
 # ZSH Plugins list
 
-plugins=(
-    git
-    autojump
-    jsontools
-    node
-    web-search
-    sudo
-    copypath
-    copyfile
-    copybuffer
-    history
-)
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
+
+# Add in snippets
+zinit snippet OMZL::git.zsh
+zinit snippet OMZP::git
+zinit snippet OMZP::sudo
+zinit snippet OMZP::archlinux
+zinit snippet OMZP::aws
+zinit snippet OMZP::kubectl
+zinit snippet OMZP::kubectx
+zinit snippet OMZP::command-not-found
+
+# Loads completions
+autoload -U compinit && compinit
