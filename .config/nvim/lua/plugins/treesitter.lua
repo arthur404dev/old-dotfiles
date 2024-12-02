42abbb4a97b87d54a71b876e639ee477e2078488
Arthur Andrade <git@arthur404.dev>
2024-12-02 10:46:22 -0300

    feat: update treesitter configs and fix highlighting
    fixes highlighting by extending the config, not directly overwriting the
    table
    adds commonly used langs to dep list

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,64 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    config = function()
-      vim.filetype.add({
-        filename = {
-          ["catppuccin.conf"] = "tmux",
-          ["sesh.conf"] = "tmux",
-          ["rose_pine.conf"] = "tmux",
-        },
-      })
-    end,
     opts = {
       ensure_installed = {
+        "astro",
         "bash",
-        "ini",
+
+        "cmake",
+        "cpp",
+        "css",
+        "diff",
+        "dockerfile",
+        "gitignore",
         "go",
-        "tmux",
+        "gomod",
+        "gosum",
+        "gowork",
+        "html",
+        "http",
+        "javascript",
+        "jsdoc",
+        "json",
+        "json5",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "prisma",
+        "python",
+        "query",
+        "regex",
+        "sql",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
       },
+      config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+
+        vim.filetype.add({
+          filename = {
+            ["catppuccin_round.conf"] = "tmux",
+            ["catppuccin_bar.conf"] = "tmux",
+            ["sesh.conf"] = "tmux",
+            ["rose_pine.conf"] = "tmux",
+          },
+        })
+        -- MDX
+        vim.filetype.add({
+          extension = {
+            mdx = "mdx",
+          },
+        })
+        vim.treesitter.language.register("markdown", "mdx")
+      end,
     },
   },
 }
