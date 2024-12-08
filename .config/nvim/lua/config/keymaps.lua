b8cd5e188772acd8273f6d741d7c89f9991fb76f
Arthur Andrade <git@arthur404.dev>
2024-12-08 00:54:00 -0300

    feat: update nvim keymaps
    adds keymap identifier
    deletes Keypgr from which-key and keybinds

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,11 @@
+local keymap = vim.keymap
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
 -- vim.keymap.del("n", "<leader>w|")
 -- vim.keymap.del("n", "<leader>|")
-vim.keymap.set("n", "<leader>a", "gg0VG", { desc = "Select all", remap = true })
-vim.keymap.set("n", "gh", "K", { desc = "Show Hover", remap = true })
+keymap.set("n", "<leader>z", "gg0VG", { desc = "Select all", remap = true })
+keymap.set("n", "gh", "K", { desc = "Show Hover", remap = true })
+
+keymap.del("n", "<leader>K")
