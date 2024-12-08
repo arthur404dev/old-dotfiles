c821eadbddba87641db8be27346eb154d91b4214
Arthur Andrade <git@arthur404.dev>
2024-12-08 04:21:24 -0300

    feat: add grapple keymaps
    adds tag direct acquisition keymaps
    adds reset keymap
    group keys using which-key

--- .config/nvim/lua/plugins/grapple.lua
+++ .config/nvim/lua/plugins/grapple.lua
@@ -10,10 +10,17 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     cmd = "Grapple",
     keys = {
-      { "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
-      { "<leader>M", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple open tags window" },
-      { "<leader>]", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
-      { "<leader>[", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+      { "<leader>ma", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
+      { "<leader>mm", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple open tags window" },
+      { "<leader>md", "<cmd>Grapple reset<cr>", desc = "Grapple reset tags" },
+      { "<leader>[", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
+      { "<leader>]", "<cmd>Grapple cycle_tass prev<cr>", desc = "Grapple cycle previous tag" },
+
+      { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+      { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+      { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+      { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+      { "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Select fifth tag" },
     },
   },
 }
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -5,6 +5,7 @@ return {
       spec = {
         { "<leader>a", group = "ai", icon = { icon = "", color = "yellow" } },
         { "<leader>z", group = "custom", icon = { icon = "", color = "cyan" } },
+        { "<leader>m", group = "grapple" },
       },
     },
   },
